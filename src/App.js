ed0dc992eaec8e751cc9ef57efac78db7f747c64
labiagamichelle29-oss <labiagamichelle29@gmail.com>
2025-08-21 23:07:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sample project
+          sample project update
         </p>
         <a
           className="App-link"
